c01bab2bd74c6dfc5e250672deb298cc63e297bc
Can Bican <can@ripe.net>
2005-02-02 14:42:20 +0000

    useless file

--- sample-build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-./configure \
-  --with-cclientinc=/export/can/src/imap-2000a/c-client \
-  --with-cclientlib=/export/can/src/imap-2000a/c-client \
-  --with-rdns \
-  --with-warnings \
-  --prefix=/home/dbase
