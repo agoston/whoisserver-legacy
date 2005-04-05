11623d523976517e5be6bf0454287bc1397824f5
Can Bican <can@ripe.net>
2005-04-05 14:17:53 +0000

    added a warning about the automake version

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,19 @@ myFail() {
   exit 1
 }
 
+# I don't know a better way for this. Please replace as appropriate.
+automake_version=`automake --version \
+  | grep ^automake \
+  | sed -e 's/^.* \([^ ][^ ]*\)$/\1/'`
+if [ "$automake_version" != "1.5" ]
+then
+  echo ===============================
+  echo Automake v1.5 is used for building.
+  echo Your version is ${automake_version}.
+  echo If configuration fails, keep this in mind :\)
+  echo ===============================
+fi
+
 echo "Running aclocal"
 aclocal -I m4 || myFail aclocal
 
