721f350275f3753965a2671aa15accb4a50995c1
Katie Petrusha <katie@ripe.net>
2006-12-20 15:33:25 +0000

    *** empty log message ***

--- src/defs/syntax_tab.h
+++ src/defs/syntax_tab.h
@@ -950,7 +950,7 @@ syntax_t syntax_tab[] = {
         /* core_parser */
         NULL,
         /* front_end_regex_pattern */
-        "^(IANA|RIR|NIR|LIR|NON-REGISTRY)$",
+        "^(IANA|RIR|NIR|LIR|OTHER)$",
         /* front_end_regex, set by syntax_init() */
         NULL,
         /* front_end_reserved_regex_pattern */
