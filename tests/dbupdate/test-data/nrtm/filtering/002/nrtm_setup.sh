25b6f1de3c5a066eaf50f03da8f5c33235f8c5fc
Agoston Horvath <agoston@ripe.net>
2008-01-22 09:46:47 +0000

    Test cases for nrtm. Kind of unusual, I know, kind of hacky, but in the end, managed to squeeze it into the dbupdate framework. They do the job and cover the nrtm code very nicely. BugzID: 45

---
+++ tests/dbupdate/test-data/nrtm/filtering/002/nrtm_setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+
+. $DATADIR/nrtm/filtering/functions.sh
+
+# insert entry to aaa table
+mysql -h$RIPADMIN_DBHOST -P$RIPADMIN_DBPORT -u$RIPADMIN_DBUSER -p$RIPADMIN_DBPASS $RIPADMIN_DBNAME <<EOM
+UPDATE aaa set mirror=2;
+EOM
+
+# have server re-read aaa cache
+issue_command set aaa_cache
---
+++ tests/dbupdate/test-data/nrtm/filtering/functions.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+# set the variables for the ripadmin database
+for i in `echo $RIPADMIN | tr "," " "`; do
+	if [ -z $RIPADMIN_DBHOST ]; then export RIPADMIN_DBHOST=$i; continue; fi
+	if [ -z $RIPADMIN_DBPORT ]; then export RIPADMIN_DBPORT=$i; continue; fi
+	if [ -z $RIPADMIN_DBUSER ]; then export RIPADMIN_DBUSER=$i; continue; fi
+	if [ -z $RIPADMIN_DBPASS ]; then export RIPADMIN_DBPASS=$i; continue; fi
+	if [ -z $RIPADMIN_DBNAME ]; then export RIPADMIN_DBNAME=$i; continue; fi
+done
+
+# issue a single command to the server
+function issue_command() {
+	echo "$@" | nc -q 1 $WHOIS_HOST $SVCONFIG_PORT
+}
+
+function issue_command_get_ret() {
+        return `echo "$@" | nc -q 1 $WHOISHOST $SVCONFIG_PORT | sed 's/.*=\([0-9]*\)=.*/\1/g' | tail -1`
+}
+
+function get_nrtm() {
+	echo "$@" | nc -q 1 $WHOIS_HOST $SVMIRROR_PORT
+}
