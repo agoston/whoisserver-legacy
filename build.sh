fdb02f1488c43ae075b64dfb2efb79386f107129
Agoston Horvath <agoston@ripe.net>
2008-05-14 09:05:17 +0000

    build.sh to support 64bit compilation

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
-cclientinc=`pwd`/third-party/c-client
-cclientlib=`pwd`/third-party/c-client
+if [ "`uname -m`" = x86_64 ]; then
+	cclientinc=`pwd`/third-party/imap-2004g-64bit/c-client
+	cclientlib=`pwd`/third-party/imap-2004g-64bit/c-client
+else
+	cclientinc=`pwd`/third-party/c-client
+	cclientlib=`pwd`/third-party/c-client
+fi
 
 ./configure --with-rdns --prefix=$HOME/local --with-cclientinc=$cclientinc --with-cclientlib=$cclientlib
