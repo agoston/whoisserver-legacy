4c9ce2423bd2631be2e238c6b11808285d6b3dad
Agoston Horvath <agoston@ripe.net>
2009-02-03 14:50:35 +0000

    fixed comment

--- src/modules/lu/lu_whois.c
+++ src/modules/lu/lu_whois.c
@@ -451,7 +451,7 @@ GList *p;
   server   - connection information
   objects  - used to return a list of objects, if any
   types    - list of objects types to be returned, e.g. "person", "inetnum", may be NULL
-  key      - lookup key, e.g. "NC123-RIPE" or "193.0.1.0/24AS2.3333"
+  key      - lookup key, e.g. "NC123-RIPE" or "193.0.1.0/24AS3333"
   inverse_keys - list of inverse keys to be looked up, NOT NULL
   source   - source, e.g. "RIPE"
 
