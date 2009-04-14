c77ab39587d3a6b07ed25b1f97aba758d93d780a
Agoston Horvath <agoston@ripe.net>
2009-04-14 14:16:48 +0000

    - rewrote km_file_to_char() - changed orders of functions to avoid having forward declarations

--- src/modules/km/km_pgp.c
+++ src/modules/km/km_pgp.c
@@ -12,9 +12,9 @@
         tiago (10/04/2003) Created.
   ******************/ /******************
   Copyright (c) 2003               RIPE NCC
- 
+
   All Rights Reserved
-  
+
   Permission to use, copy, modify, and distribute this software and its
   documentation for any purpose and without fee is hereby granted,
   provided that the above copyright notice appear in all copies and that
@@ -22,7 +22,7 @@
   supporting documentation, and that the name of the author not be
   used in advertising or publicity pertaining to distribution of the
   software without specific, written prior permission.
-  
+
   THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING
   ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS; IN NO EVENT SHALL
   AUTHOR BE LIABLE FOR ANY SPECIAL, INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY
@@ -49,7 +49,28 @@ static gchar* gpg_path = NULL;
 static LG_context_t* ctx;
 static GList* my_sources = NULL;
 
-void km_pgp_end();
+void km_pgp_end() {
+  GList* tmp_list;
+
+  if (my_sources) {
+    tmp_list = my_sources;
+    while(tmp_list) {
+      g_free(tmp_list->data);
+      tmp_list = tmp_list->next;
+    }
+    g_list_free(my_sources);
+    my_sources = NULL;
+  }
+  if (temporary_directory) {
+    g_free(temporary_directory);
+    temporary_directory = NULL;
+  }
+  if (gpg_path) {
+    g_free(gpg_path);
+    gpg_path = NULL;
+  }
+}
+
 /*
   Initializes the PGP sub-system.
 
@@ -71,30 +92,6 @@ KM_status_t km_pgp_init(LG_context_t* _ctx, GList* servers,
   return KM_OK;
 }
 
-void km_pgp_end() {
-  GList* tmp_list;
-  
-  if (my_sources) {
-    tmp_list = my_sources;
-    while(tmp_list) {
-      g_free(tmp_list->data);
-      tmp_list = tmp_list->next;
-    }
-    g_list_free(my_sources);
-    my_sources = NULL;
-  }
-  if (temporary_directory) {
-    g_free(temporary_directory);
-    temporary_directory = NULL;
-  }
-  if (gpg_path) {
-    g_free(gpg_path);
-    gpg_path = NULL;
-  }
-
-
-}
-
 
 /*
   Returns a char with the content of a file (should be text).
@@ -103,82 +100,27 @@ void km_pgp_end() {
 
   return - The file content as a NULL terminated string.
  */
-static gchar* km_file_to_char(FILE *file) {
-  GString* g;
-  char buf[100];
-  gchar* ret;
+static gchar* km_file_to_char(FILE *file)
+{
+    GString *g = g_string_sized_new(16384);
+    gchar buf[4096];
+    int len;
 
-  g = g_string_new("");
-  do {
-    fgets(buf, 99, file);
-    if (!feof(file)) {
-       g_string_append(g, buf);
+    do
+    {
+        len = fread(buf, 4096, 1, file);
+        g_string_append_len(g, buf, len);
     }
-   } while (!feof(file));
-  ret = g->str;
-  g_string_free(g, FALSE);
-  return ret;
-}
+    while (len < 4096);
 
-KM_key_return_t* km_pgp_signature_verify_low(gchar* text, gchar* signature,
-                                             gchar *key_ring);
+    return g_string_free(g, FALSE);
+}
 
 
 /*
-  Verifies a signature.
-
-  See KM_signature_verify for parameters.
+ * The low-lever signature verification. Contains all the dirty stuff.
+ * Called from this file only.
  */
-KM_key_return_t* km_pgp_signature_verify(gchar* text, gchar* signature) {
-
-#define LINE_LENGTH 400
-  KM_key_return_t* ret;
-  gchar* key;
-  gchar key_file[LINE_LENGTH];
-  gchar nic[LINE_LENGTH];
-  gchar tmp[LINE_LENGTH];
-  KM_status_t ret_status;
-
-  LG_log(ctx, LG_FUNC, ">Entering km_pgp_signature_verify");
-  if (temporary_directory) {
-    strcpy(tmp, temporary_directory);
-  }
-  else {
-    strcpy(tmp, "/tmp");
-  }
-  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: call km_pgp_signature_verify_low with no key ring");
-  ret = km_pgp_signature_verify_low(text, signature, NULL);
-  ret_status = KM_key_return_get_status(ret);
-  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: status [%s]", KM_return_string(ret_status));
-  if (ret_status != KM_OK) {
-    LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
-    return ret;
-  }
-  //printf("AAAAA%s\n", KM_key_return_get_key_id(ret));
-  sprintf(nic, "PGPKEY-%s",  KM_key_return_get_key_id(ret));
-  key = KM_lu_get(nic);
-  if (key == NULL) {
-    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: key-cert object %s not found", nic);
-    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: set FALSE cred with KM_UNEXISTENT_KEY status");
-    LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
-    return km_key_return_new(NULL, FALSE, NULL, KM_UNEXISTENT_KEY);
-  }
-  sprintf(key_file,"%s/km_key_%d", tmp, (int)(getpid()) );
-
-  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: add key to key ring");
-  ret = km_pgp_key_add_internal(key, key_file);
-  if (KM_key_return_get_status(ret) == KM_OK) {
-    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: call km_pgp_signature_verify_low with key ring");
-    ret = km_pgp_signature_verify_low(text, signature, key_file);
-  }
-
-  unlink(key_file);
-  strcat(key_file, "~");
-  unlink(key_file);
-  LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
-  return ret;
-}
-
 KM_key_return_t* km_pgp_signature_verify_low(gchar* text, gchar* signature, gchar *key_ring) {
   km_key_return_t* key_ret;
   FILE* general;
@@ -248,7 +190,7 @@ KM_key_return_t* km_pgp_signature_verify_low(gchar* text, gchar* signature, gcha
     LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify_low: gpg returns: %s", txt);
     if (strstr(txt, "Good signature") != NULL)
     {
-      valid = TRUE; 
+      valid = TRUE;
       LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify_low: Good signature found");
     }
 
@@ -300,6 +242,63 @@ KM_key_return_t* km_pgp_signature_verify_low(gchar* text, gchar* signature, gcha
   }
   LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify_low");
   return key_ret;
+}
+
+
+
+/*
+  Verifies a signature.
+
+  See KM_signature_verify for parameters.
+ */
+KM_key_return_t* km_pgp_signature_verify(gchar* text, gchar* signature) {
+
+#define LINE_LENGTH 400
+  KM_key_return_t* ret;
+  gchar* key;
+  gchar key_file[LINE_LENGTH];
+  gchar nic[LINE_LENGTH];
+  gchar tmp[LINE_LENGTH];
+  KM_status_t ret_status;
+
+  LG_log(ctx, LG_FUNC, ">Entering km_pgp_signature_verify");
+  if (temporary_directory) {
+    strcpy(tmp, temporary_directory);
+  }
+  else {
+    strcpy(tmp, "/tmp");
+  }
+  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: call km_pgp_signature_verify_low with no key ring");
+  ret = km_pgp_signature_verify_low(text, signature, NULL);
+  ret_status = KM_key_return_get_status(ret);
+  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: status [%s]", KM_return_string(ret_status));
+  if (ret_status != KM_OK) {
+    LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
+    return ret;
+  }
+  //printf("AAAAA%s\n", KM_key_return_get_key_id(ret));
+  sprintf(nic, "PGPKEY-%s",  KM_key_return_get_key_id(ret));
+  key = KM_lu_get(nic);
+  if (key == NULL) {
+    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: key-cert object %s not found", nic);
+    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: set FALSE cred with KM_UNEXISTENT_KEY status");
+    LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
+    return km_key_return_new(NULL, FALSE, NULL, KM_UNEXISTENT_KEY);
+  }
+  sprintf(key_file,"%s/km_key_%d", tmp, (int)(getpid()) );
+
+  LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: add key to key ring");
+  ret = km_pgp_key_add_internal(key, key_file);
+  if (KM_key_return_get_status(ret) == KM_OK) {
+    LG_log(ctx, LG_DEBUG, "km_pgp_signature_verify: call km_pgp_signature_verify_low with key ring");
+    ret = km_pgp_signature_verify_low(text, signature, key_file);
+  }
+
+  unlink(key_file);
+  strcat(key_file, "~");
+  unlink(key_file);
+  LG_log(ctx, LG_FUNC, "<Exiting km_pgp_signature_verify");
+  return ret;
 }
 
 
@@ -372,18 +371,18 @@ KM_key_return_t* km_pgp_key_add_internal(gchar* key, gchar *key_ring) {
   while (fgets (txt, LINE_LENGTH - 1, general) != NULL) {
     LG_log(ctx, LG_DEBUG, "%s", txt);
     if (strstr(txt, "public key") != NULL) {
-      if ((strstr(txt, "unchanged") != NULL) || 
+      if ((strstr(txt, "unchanged") != NULL) ||
           (strstr(txt, "not changed") != NULL)) {
-        snprintf(key_id, LINE_LENGTH - 1, "%8s\n", txt + 9); 
+        snprintf(key_id, LINE_LENGTH - 1, "%8s\n", txt + 9);
         key_id[8] = 0;
         unchanged++;
       }
       if(strstr(txt, "imported") != NULL) {
-        snprintf(key_id, LINE_LENGTH -1, "%8s\n", txt + 9); 
+        snprintf(key_id, LINE_LENGTH -1, "%8s\n", txt + 9);
         key_id[8] = 0;
         imported++;
       }
-    } 
+    }
     if (strstr(txt, "secret key") != NULL ||
         strstr(txt, "failed to create temporary file") != NULL ) {
         //this falied stuff has to do with using /dev/null its OK
@@ -486,10 +485,10 @@ void km_pgp_key_get_fingerprint(km_key_return_t* kr, gchar *key_ring) {
 
   if (finger_print[0]) {
     km_key_return_add(kr, "finger_print", finger_print);
-  } 
+  }
   if (key_owner[0]) {
     km_key_return_add(kr, "key_owner", key_owner);
-  } 
+  }
 }
 
 KM_key_return_t* km_pgp_add_check(gchar* key, gboolean get_information) {
