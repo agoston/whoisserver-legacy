ca569971752e106cf4dddaa1ee1c537710334d3c
Agoston Horvath <agoston@db-dev1.ripe.net>
2009-08-11 17:45:06 +0200

    fixed index (id was position +1, grrr)

--- src/defs/Defs.java
+++ src/defs/Defs.java
@@ -771,7 +771,7 @@ public class Defs {
             // look up the class ID
             int index = ripeClassAliases.indexOf(ad.getForeignkey());
             if (index >= 0) {
-                out.println("        "+index+",    /* "+ad.getForeignkey()+" */");
+                out.println("        "+(index+1)+",    /* "+ad.getForeignkey()+" */");
             } else {
                 System.err.println("Attribute "+ad.getName()+" has invalid foreign key specified!");
                 System.exit(1);
