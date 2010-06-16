ba3361b0ff034be8413b2fb3b839bd80aa441911
Agoston Horvath <agoston@ripe.net>
2010-06-16 17:08:13 +0200

    update-git

--- src/utils/wrapper.sh
+++ src/utils/wrapper.sh
@@ -34,12 +34,27 @@ fi
 # wrap dbupdate call - note that this is delibaretly using $*, and not "$@", as some arguments passed
 # from perl over ssh "might" get concatenated, and this is the only change to separate them.
 # We never have spaces in any of the arguments here, so it's fine - agoston, 2010-06-07
-${DBUPDATE} $*
+INFILE=`tempfile`
+OUTFILE=`tempfile`
+ERRFILE=`tempfile`
+cat >$INFILE
+${DBUPDATE} $* <$INFILE >$OUTFILE 2>$ERRFILE
 RET=$?
+cat $OUTFILE
+cat $ERRFILE 1>&2
 
 # send email on non-zero return code
 if [ $RET -gt 0 ]; then
 	echo "${DBUPDATE} exited with code ${RET} on ${HOSTNAME}
-Command line: ${DBUPDATE} $@" | mail -s "${DBUPDATE} execution problem" $ERRORS_TO
-	exit $RET
+Command line: ${DBUPDATE} $@
+
+STDIN: `cat $INFILE`
+
+STDOUT: `cat $OUTFILE`
+
+STDERR: `cat $ERRFILE`
+" | mail -s "${DBUPDATE} execution problem" $ERRORS_TO
 fi
+
+rm -f $INFILE $OUTFILE $ERRFILE
+exit $RET
