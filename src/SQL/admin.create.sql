1eaaa9c7b6097797cf53b89498e7a86bf899ad28
Agoston Horvath <agoston@db-dev1.ripe.net>
2010-12-28 13:44:14 +0100

    'backported' src/SQL from strong_reg (proved very useful)

--- src/SQL/admin.create.sql
+++ src/SQL/admin.create.sql
@@ -1,3 +1,14 @@
+DROP TABLE IF EXISTS aaa;
+CREATE TABLE aaa (
+  prefix int(10) unsigned NOT NULL default '0',
+  prefix_length tinyint(3) unsigned NOT NULL default '0',
+  source varchar(32) NOT NULL default '',
+  ripupdate tinyint(3) NOT NULL default '0',
+  mirror tinyint(3) NOT NULL default '0',
+  comment longblob,
+  PRIMARY KEY  (prefix,prefix_length,source)
+);
+
 DROP TABLE IF EXISTS aaa6;
 CREATE TABLE aaa6 (
   prefix1 int(10) unsigned NOT NULL default '0',
@@ -12,8 +23,23 @@ CREATE TABLE aaa6 (
   PRIMARY KEY  (prefix1,prefix2,prefix3,prefix4,prefix_length,source)
 );
 
-INSERT INTO aaa6 VALUES (0, 0, 0, 0, 0, 'DB-TEST', 1, 1, 'Default IPv6 access');
-INSERT INTO aaa6 VALUES (0, 0, 0, 0, 0, 'RIPE', 1, 1, 'Default IPv6 access');
+DROP TABLE IF EXISTS access;
+CREATE TABLE access (
+  prefix int(10) unsigned NOT NULL default '0',
+  prefix_length tinyint(3) unsigned NOT NULL default '0',
+  connections int(4) unsigned NOT NULL default '0',
+  addr_passes tinyint(3) unsigned NOT NULL default '0',
+  denials int(4) unsigned NOT NULL default '0',
+  queries int(4) unsigned NOT NULL default '0',
+  referrals int(4) unsigned NOT NULL default '0',
+  public_objects int(6) unsigned NOT NULL default '0',
+  private_objects int(6) unsigned NOT NULL default '0',
+  public_bonus double NOT NULL default '0',
+  private_bonus double NOT NULL default '0',
+  timestamp bigint(20) unsigned NOT NULL default '0',
+  PRIMARY KEY  (prefix,prefix_length),
+  KEY access_timestamp (timestamp)
+);
 
 DROP TABLE IF EXISTS access6;
 CREATE TABLE access6 (
@@ -36,6 +62,21 @@ CREATE TABLE access6 (
   KEY access_timestamp (timestamp)
 );
 
+DROP TABLE IF EXISTS acl;
+CREATE TABLE acl (
+  prefix int(10) unsigned NOT NULL default '0',
+  prefix_length tinyint(3) unsigned NOT NULL default '0',
+  maxbonus int(10) NOT NULL default '0',
+  maxpublic int(10) default '-1',
+  maxdenials int(10) unsigned NOT NULL default '0',
+  deny tinyint(3) unsigned NOT NULL default '0',
+  trustpass tinyint(3) unsigned NOT NULL default '0',
+  threshold tinyint(3) unsigned NOT NULL default '4',
+  maxconn tinyint(3) unsigned NOT NULL default '6',
+  comment longblob,
+  PRIMARY KEY  (prefix,prefix_length)
+);
+
 DROP TABLE IF EXISTS acl6;
 CREATE TABLE acl6 (
   prefix1 int(10) unsigned NOT NULL default '0',
@@ -53,5 +94,3 @@ CREATE TABLE acl6 (
   comment longblob,
   PRIMARY KEY  (prefix1,prefix2,prefix3,prefix4,prefix_length)
 );
-
-INSERT INTO acl6 VALUES (0,0,0,0,0,2000,-1,10,0,0,4,6,'Default IPv6 access');
---
+++ src/SQL/main.data.sql
@@ -0,0 +1,28 @@
+INSERT mntner SET object_id=0, mntner='ANY', dummy=1;
+
+INSERT INTO object_order VALUES (22,10);  # inet-reg
+INSERT INTO object_order VALUES (6,11);   # inetnum
+INSERT INTO object_order VALUES (23,20);  # inet6-reg
+INSERT INTO object_order VALUES (5,21);   # inet6num
+INSERT INTO object_order VALUES (0,30);   # as-block
+INSERT INTO object_order VALUES (24,31);  # as-reg
+INSERT INTO object_order VALUES (2,32);   # aut-num
+INSERT INTO object_order VALUES (1,33);   # as-set
+INSERT INTO object_order VALUES (12,40);  # route
+INSERT INTO object_order VALUES (19,41);  # route6
+INSERT INTO object_order VALUES (13,42);  # route-set
+INSERT INTO object_order VALUES (4,50);   # inet-rtr
+INSERT INTO object_order VALUES (14,60);  # filter-set
+INSERT INTO object_order VALUES (15,61);  # peering-set
+INSERT INTO object_order VALUES (16,62);  # rtr-set
+INSERT INTO object_order VALUES (3,70);   # domain
+INSERT INTO object_order VALUES (8,80);   # limerick
+INSERT INTO object_order VALUES (21,81);  # poetic-form
+INSERT INTO object_order VALUES (20,82);  # poem
+INSERT INTO object_order VALUES (9,90);   # mntner
+INSERT INTO object_order VALUES (17,91);  # irt
+INSERT INTO object_order VALUES (7,100);  # key-cert
+INSERT INTO object_order VALUES (25,110); # org-reg
+INSERT INTO object_order VALUES (18,111); # organisation
+INSERT INTO object_order VALUES (11,120); # role
+INSERT INTO object_order VALUES (10,121); # person
