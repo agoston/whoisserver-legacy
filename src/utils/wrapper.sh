cae8df685f2ac825f721aaba1978d0c7514880c4
Agoston Horvath <agoston@ripe.net>
2011-04-20 16:49:09 +0200

    update-git

--- src/utils/wrapper.sh
+++ src/utils/wrapper.sh
@@ -10,30 +10,28 @@
 # ****************************************
 #
 # wrapper around dbupdate to:
-# - process STOPUPDATES (mailupdates has its own; syncupdates, override, ... is relying on this one)
 # - handle dbupdate crash
+# - log execution time
 #
-# agoston, 2010-05-31
+# agoston, 2011-04-20
 
 # import default settings
 . /etc/default/ripe
 
-BASEDIR=/home/dbase/services/whois-update
+INSTANCE=${0##*dbupdate-}
+if ! [[ $INSTANCE ]]; then
+	echo Should be called as dbupdate-INSTANCE
+	exit 1
+fi
+
+BASEDIR=/home/dbase/services/whois-${INSTANCE}
 STOPUPDATES=${BASEDIR}/var/STOPUPDATES
-DBUPDATE=${BASEDIR}/bin/dbupdate.real
+DBUPDATE=${BASEDIR}/bin/dbupdate
 ERRORS_TO="dbint@ripe.net"
 
 # log execution times (used in CMM reports ONLY)
 TIMELOG=$BASEDIR/var/log/time/timelog.$(date +%Y%m%d)
 TIME="/usr/bin/time --append --quiet --format %e --output ${TIMELOG}"
-
-# check for STOPUPDATES
-if [ -f $STOPUPDATES ]; then
-	echo "Updates are down for maintenance at the moment. Please try again later."
-	echo "For further information, please visit the RIPE NCC Service Announcements at"
-	echo "http://ripe.net/news/status.html"
-	exit 1
-fi
 
 # wrap dbupdate call - note that this is delibaretly using $*, and not "$@", as some arguments passed
 # from perl over ssh "might" get concatenated, and this is the only change to separate them.
