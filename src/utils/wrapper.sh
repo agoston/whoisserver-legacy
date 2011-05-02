3639f1cfdc3a6b10ef6ae1cf7563718dbf49c348
Agoston Horvath <agoston@ripe.net>
2011-05-02 11:03:45 +0200

    update-git

--- src/utils/wrapper.sh
+++ src/utils/wrapper.sh
@@ -18,20 +18,12 @@
 # import default settings
 . /etc/default/ripe
 
-INSTANCE=${0##*dbupdate-}
-if ! [[ $INSTANCE ]]; then
-	echo Should be called as dbupdate-INSTANCE
-	exit 1
-fi
-
-BASEDIR=/home/dbase/services/whois-${INSTANCE}
+BASEDIR=$(readlink -e $(dirname $0))/..
 DBUPDATE=${BASEDIR}/bin/dbupdate
 ERRORS_TO="dbint@ripe.net"
 
 # log execution times (used in CMM reports ONLY)
-if [ ! -d "$BASEDIR/var/log/time" ]; then
-	mkdir $BASEDIR/var/log/time
-fi
+mkdir -p $BASEDIR/var/log/time &>/dev/null
 TIMELOG=$BASEDIR/var/log/time/timelog.$(date +%Y%m%d)
 TIME="/usr/bin/time --append --quiet --format %e --output ${TIMELOG}"
 
