bb1c47383c859096f1e36877ee1200300ab480f7
Agoston Horvath <agoston@ripe.net>
2012-08-08 12:04:22 +0200

    disabled read committed isolation until locking redesign

--- src/modules/sq/mysql_driver.c
+++ src/modules/sq/mysql_driver.c
@@ -99,10 +99,11 @@ int SQ_try_connection(SQ_connection_t **conn, const char *host, unsigned int por
         fprintf(stderr, "mysql_options failed: unknown option MYSQL_READ_DEFAULT_GROUP: %s\n", mysql_error(*conn));
         die;
     }
-    if (mysql_options(*conn, MYSQL_INIT_COMMAND, "SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;")) {
-        fprintf(stderr, "mysql_options failed: unknown option MYSQL_INIT_COMMAND: %s\n", mysql_error(*conn));
-        die;
-    }
+//    FIXME: Disabled until we fix transaction isolation and mysql replication
+//    if (mysql_options(*conn, MYSQL_INIT_COMMAND, "SET SESSION TRANSACTION ISOLATION LEVEL READ COMMITTED;")) {
+//        fprintf(stderr, "mysql_options failed: unknown option MYSQL_INIT_COMMAND: %s\n", mysql_error(*conn));
+//        die;
+//    }
 
     res = mysql_real_connect(*conn, host, user, password, db, port, NULL, 0);
     if (res == NULL) {
